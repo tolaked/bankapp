2506d04362fbd25c712f35518f486e0da038eed0
tolaked1 <adetolaakere1@gmail.com>
2023-03-20 20:47:18 +0000

    create signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import './App.css'
+import "./App.css";
+import Signup from "./components/Signup/SignUp";
 
 function App() {
   return (
     <Routes>
-    <Route path="/" element={<Dashboard/>}/>
-   </Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
